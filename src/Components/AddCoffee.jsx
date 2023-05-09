1d1aaefb98e4be28e462a1a97106bfed61e7628a
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-09 07:19:25 +0600

    first commit

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">Add a coffee</h3>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/Components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+             <h3 className="text-4xl">Update a coffee</h3>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
